a36dcfd4b143972a5481b3243d532b64d3baa6e8
iueno <178707868+iueno@users.noreply.github.com>
2024-09-12 01:24:21 +0900

    initial commit

---
+++ src/main/java/com/uelab/experiment/ExperimentApplication.java
@@ -0,0 +1,18 @@
+package com.uelab.experiment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExperimentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
